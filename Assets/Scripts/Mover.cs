951ead8fe1f6e5f92c653ed4aa06dd749f36bede
jwabend <63935497+jwabend@users.noreply.github.com>
2024-07-31 12:39:33 -0400

    Colored Walls When Bumped
    Changed the color of walls when bumped, cleaned up code

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -10,16 +10,26 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-        
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {   
+        PlayerControls();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD");
+        Debug.Log("Don't hit the walls");
+    }
+
+    void PlayerControls()
+    {
         float xMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float zMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Translate(xMovement,0,zMovement); 
     }
-
 }
